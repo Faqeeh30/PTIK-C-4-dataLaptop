e518f68cd4743b1b72a194f9935c23d1bd8bf208
Faqeeh30 <150775256+Faqeeh30@users.noreply.github.com>
2023-12-15 08:53:01 +0700

    melihat data
    Menambahkan fitur melihat data oleh rima

--- PTIKCrued.cpp
+++ PTIKCrued.cpp
@@ -48,6 +48,17 @@ void tambahData(vector<laptop>& data) {
 }
 
 //Tampilkan data
+void tampilkanData(const vector<laptop>& data) {
+  if (data.empty()) {
+    cout << "Tidak ada data laptop.\n";
+    return;
+  }
+  cout << "Data laptop yang tersimpan:\n";
+  cout << "No.\tBrend\tSerialLaptop\tProcessor\tRAM(GB)\tROM(GB)\tPanel\tDaya\tHarga(rp)\n";
+  for (int i = 0; i < data.size(); i++) {
+    cout << i + 1 << "\t" << data[i].brand << "\t" << data[i].seri << "\t" << data[i].processor << "\t" << data[i].ram << "\t" << data[i].rom << "\t" << data[i].panel << "\t" << data[i].daya << "\t" << data[i].harga << "\t" << "\n";
+  }
+}
 
 //Ubah data
 void ubahData(vector<laptop>& data) {
